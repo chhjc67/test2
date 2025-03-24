b38db5f856713e5c0a6b1e2d12b32ff71780af87
Juan Carlos Chaves Hernandez <chhjc@hotmail.com>
2025-03-24 09:28:07 -0500

    Envia cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
 import { GeneralComponent } from './components/test/general/general.component';
-import { ProfileComponent } from './components/test/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GeneralComponent, ProfileComponent],
-  template: `<router-outlet />
+  imports: [RouterOutlet, GeneralComponent],
+  template: `
+    <router-outlet />
     <app-general
       [inputParameter]="message"
       (messageEvent)="reciveMessage($event)"
@@ -16,11 +16,11 @@ import { ProfileComponent } from './components/test/profile/profile.component';
       <p selector1>usando ng-content (selector1)</p>
       <p selector2>usando ng-content (selector2)</p>
     </app-general>
-    <app-profile [inputParameter]="message"></app-profile>`,
+  `,
   styles: [],
 })
 export class AppComponent implements AfterViewInit {
-  public menuItems = routes
+  menuItems = routes
     .map((route) => route.children ?? [])
     .flat()
     .filter((route) => route && route.path)
@@ -29,16 +29,17 @@ export class AppComponent implements AfterViewInit {
   @ViewChild(GeneralComponent) viewChildObj: any;
 
   constructor() {
-    console.log('Log 1:' + this.menuItems);
+    console.log('Log 1:', this.menuItems);
   }
 
   ngAfterViewInit() {
     console.log(
-      'Mensaje recido desde componente hijo...' + this.viewChildObj.userName
+      'Mensaje recido desde componente hijo...',
+      this.viewChildObj.userName
     );
   }
 
-  reciveMessage(message: string) {
-    console.log('Mensaje recido desde componente hijo por evento...' + message);
+  reciveMessage(number: string) {
+    console.log('Mensaje recido desde componente hijo por evento...', number);
   }
 }
